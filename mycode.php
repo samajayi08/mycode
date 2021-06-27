523e5c68cf4861c22846f6cbc866beeedee2115a
samajayi08 <86532823+samajayi08@users.noreply.github.com>
2021-06-27 16:29:59 +0100

    Add files via upload

---
+++ mycode.php
@@ -0,0 +1,26 @@
+<?php
+ 
+ $num1 = range(1, 50);
+
+print_r($num1);
+echo  "<br>";
+echo  "<br>";
+echo  "<br>";
+
+ $num2 = range(51, 100);
+
+print_r($num2);
+
+
+
+echo  "<br>";
+echo  "<br>";
+echo  "<br>";
+
+
+$ar = array("a" =>23, "b" =>45, "c" =>67);
+
+echo array_sum($ar);
+
+
+
